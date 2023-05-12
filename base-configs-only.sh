e84bad5c8d87a7ec7195d3e261ba26be85e49a1f
MichaelSebero <greenlightsuccess@protonmail.com>
2023-05-12 10:21:18 -0400

    .

--- base-configs-only.sh
+++ base-configs-only.sh
@@ -9,8 +9,6 @@ cat confs/etc-aide-conf > /etc/aide.conf
 cat confs/etc-bash-bashrc > /etc/bash.bashrc
 cat confs/etc-conf-d-wireless-regdom > /etc/conf.d/wireless-regdom
 cat confs/etc-crypttab > /etc/crypttab
-cat confs/etc-default-passwd > /etc/default/passwd
-cat confs/etc-default-useradd > /etc/default/useradd
 cat confs/etc-dhclient-conf > /etc/dhclient.conf
 cat confs/etc-environment > /etc/environment
 cat confs/etc-fstab > /etc/fstab
@@ -21,14 +19,11 @@ cat confs/etc-issue > /etc/issue
 cat confs/etc-issue-net > /etc/issue.net
 cat confs/etc-locale-conf > /etc/locale.conf
 cat confs/etc-locale-gen > /etc/locale.gen
-cat confs/etc-login-defs > /etc/login.defs
 cat confs/etc-logrotate-conf > /etc/logrotate.conf
-cat confs/etc-lynis-default-prf > /etc/lynis/default.prf
 cat confs/etc-mkinitcpio-conf > /etc/mkinitcpio.conf
 cat confs/etc-modprobe-d-blacklist-firewire > /etc/modprobe.d/blacklist-firewire
 cat confs/etc-modprobe-d-blacklist-usb > /etc/modprobe.d/blacklist-usb
 cat confs/etc-motd > /etc/motd
-cat confs/etc-pacman-d-mirrorlist > /etc/pacman.d/mirrorlist
 cat confs/etc-profile > /etc/profile
 cat confs/etc-securetty > /etc/securetty
 cat confs/etc-security-access-conf > /etc/security/access.conf
@@ -41,8 +36,6 @@ cat confs/etc-shells > /etc/shells
 cat confs/etc-ssh-ssh_config > /etc/ssh/ssh_config
 cat confs/etc-ssh-sshd_config > /etc/ssh/sshd_config
 cat confs/etc-sudoers > /etc/sudoers
-cat confs/etc-sysctl-d-05-grsecurity-conf > /etc/sysctl.d/05-grsecurity.conf
-cat confs/etc-sysctl-d-10-sysctl-conf > /etc/sysctl.d/10-sysctl.conf
 cat confs/etc-vconsole-conf > /etc/vconsole.conf
 cat confs/etc-wpa_supplicant-wpa-supplicant-conf > /etc/wpa_supplicant/wpa-supplicant.conf
 cat confs/etc-zsh-zprofile > /etc/zsh/zprofile
